c89a807cd5d98b741bd8da0c678f9ed16594936f
Ghost <vladimirvsevolodovi@yandex.ru>
2021-06-29 15:30:03 +0300

    git: add src/index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const UltraPromise = require("./ultraPromise");
+const UltraPromiseW = require("./ultraPromiseW");
+const UltraPromiseWrapper = require("./ultraPromiseWrapper");
+
+module.exports = {
+	
+	"default": UltraPromiseWrapper,
+	UltraPromise,
+	UltraPromiseW
+	
+};
